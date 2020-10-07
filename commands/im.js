2851808a56ee7f4606342b178fe9047776f2a502
gurrrrrrett3 <61361654+gurrrrrrett3@users.noreply.github.com>
2020-10-07 17:22:48 -0400

    code

--- commands/im.js
+++ commands/im.js
@@ -2,14 +2,17 @@ module.exports = {
     name: 'im',
     description: "this is a response command!",
     execute(message, args){
-   
-        if (Math.floor(Math.random()) * 3 == 2){
+   var rand = Math.floor(Math.random() * 3)
+         message.channel.send(rand);
+        if (rand == 2){
 
          const mess = message.content;
           const pos = mess.indexOf('im') + 3;
           const out = 'Hi, ' + mess.substring(pos,mess.length) + ", I'm Toni!";
          message.channel.send(out);
         }
-        else return;
+        else 
+        
+        return;
     }
 }
