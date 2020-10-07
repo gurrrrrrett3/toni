1bd9dccb9192d55a368146c1d9a9740ad28566be
astralamari <elijahanest@gmail.com>
2020-10-07 17:45:45 -0400

    new
    added more responses to im.js

--- commands/im.js
+++ commands/im.js
@@ -3,12 +3,33 @@ module.exports = {
     description: "this is a response command!",
     execute(message, args){
    var rand = Math.floor(Math.random() * 3)
-         message.channel.send(rand);
         if (rand == 2){
+
+            const tonyPhrases = [ 
+                ", I'ma Toni!",
+                ", I'ma Toni!",
+                ", I'ma Toni!",
+                ", I'ma Toni!",
+                ", I'ma Toni!",
+                ", I'ma Toni!",
+                ", I'ma Toni!",
+                ", I'ma Toni!",
+                ", I'ma making pasta!",
+                ", I'ma YOUR dad!",
+                ", I'ma making pizza!",
+                ", I'ma eating canolis!",
+                ", I'ma fucking balling!",
+                ", I'ma evaluating mortality!",
+                ", I'ma in love!",
+                ", I'ma a new man!",
+                ", I'ma playing hit new video game: team fortress 2 for the xbox 360"
+
+            ]
+            const tonySpeak = tonyPhrases[Math.floor(Math.random() * (tonyPhrases.length))]    
 
          const mess = message.content;
           const pos = mess.indexOf('im') + 3;
-          const out = 'Hi, ' + mess.substring(pos,mess.length) + ", I'm Toni!";
+          const out = 'Hi, ' + mess.substring(pos,mess.length) + tonySpeak;
          message.channel.send(out);
         }
         else 
