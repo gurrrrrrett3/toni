ba223b0c3cab5dd6684702be006169205dc5d9ff
gurrrrrrett3 <61361654+gurrrrrrett3@users.noreply.github.com>
2020-10-14 14:48:25 -0400

    Fixed lvl

--- commands/lvl.js
+++ commands/lvl.js
@@ -3,8 +3,16 @@ module.exports = {
     description: "this shows lvl data!",
     execute(message, args){
        const fs = require('fs')
-       
-       
-        message.channel.send('pong!');
+       const jsonfile = require('jsonfile')
+
+       if (fs.existsSync('stats.json')) {
+        stats = jsonfile.readFileSync('stats.json');
+
+        const userStats = stats[message.author.id];
+        const xpToNextLevel = (5* Math.pow(userStats.level, 2) + 50 * userStats.level + 100) - userStats.xp;
+        const out = "```" + message.author.username.toUpperCase() + "'S STATS \nLevel: "+ userStats.level + "\nxp: " + userStats.xp + "\nYou need "+ xpToNextLevel + " more xp points to level up!```"
+    
+        message.channel.send(out)
+        }
     }
 }
