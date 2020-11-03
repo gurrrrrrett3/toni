9b91f28388116fb63a007334bc1820b186d57a1c
gurrrrrrett3 <61361654+gurrrrrrett3@users.noreply.github.com>
2020-11-03 13:51:19 -0500

    Now I fixed the null error

--- commands/speedtop.js
+++ commands/speedtop.js
@@ -21,6 +21,17 @@ module.exports = {
             return;
         }
 
+        //attempt to fix null error
+
+        speedArray.forEach(element => {
+            if (element.average == null) {
+                element.average = 999999
+                console.log("Found null average for " + element.name + ", fixed error")
+            } else {
+                console.log("Scanned  " + element.name + ", no error")
+            }
+        })
+
         const out = "***TOP 10 SPEEDY BOIS***\n" + "1. " +  speedArray[0].name + ": Average " + speedArray[0].average + "ms\n2. " +  speedArray[1].name + ": Average " + speedArray[1].average + "ms\n3. " +  speedArray[2].name + ": Average " + speedArray[2].average + "ms\n4. " +  speedArray[3].name + ": Average " + speedArray[3].average + "ms\n5. " +  speedArray[4].name + ": Average " + speedArray[4].average + "ms\n6. " +  speedArray[5].name + ": Average " + speedArray[5].average + "ms\n7. " +  speedArray[6].name + ": Average " + speedArray[6].average + "ms\n8. " +  speedArray[7].name + ": Average " + speedArray[7].average + "ms\n9. " +  speedArray[8].name + ": Average " + speedArray[8].average + "ms\n10. " +  speedArray[9].name + ": Average " + speedArray[9].average
 
         message.channel.send(out);
