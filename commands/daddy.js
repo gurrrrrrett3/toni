d66df2a6a5af0a8dbd2cf5dfe26794f78449097a
gurrrrrrett3 <61361654+gurrrrrrett3@users.noreply.github.com>
2021-03-02 18:20:40 -0500

    Goodbye Old Toni

--- commands/daddy.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'daddy',
-    description: "this is a ping command!",
-    execute(message, args){
-        message.channel.send('https://media.discordapp.net/attachments/744632667068301515/763212399812673547/image0.png?width=77&height=135');
-    }
-}
