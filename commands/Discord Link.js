5fae548a7f4a9ffdfb774fc3fda446d7ea469553
gurrrrrrett3 <61361654+gurrrrrrett3@users.noreply.github.com>
2021-03-06 22:24:08 -0500

    more discord

--- commands/Discord Link.js
+++ commands/Discord Link.js
@@ -1,7 +1,16 @@
 module.exports = {
-    name: 'discord',
+    name: 'discord link',
     description: "Discord link!",
     execute(message, args){
-        message.channel.send('Here is a link to my official discord server. https://discord.gg/XK5rjrEn');
+        
+        const responses = [
+            'Here is a link to my official discord server.',
+            'Ciao, sono Toni!',
+            'Hey, have you ever heard of Discord?',
+            'Not to advertise, but...'
+        ]
+        
+        const response = (responses[Math.floor(Math.random() * responses.length)])
+        message.channel.send(`${response} https://discord.gg/XK5rjrEn`);
     }
 }
